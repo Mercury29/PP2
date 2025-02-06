aac467109b5f7ac40b2b33f58d9856a2f3c84a0b
Mercury29 <148089191+Mercury29@users.noreply.github.com>
2025-02-07 00:33:33 +0500

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,33 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+print(bool("Hello"))
+print(bool(15))
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+for x in "banana":
+    print(x)
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+for x in range(6):
+    print(x)
+a=[0,1,2]
+for x in a:
+    pass
+    print(x)
